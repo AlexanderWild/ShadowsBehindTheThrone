c2a3e85c72346de2cad4b0461a7803272671286d
Wyatt <git.wquist@gmail.com>
2020-03-12 22:18:53 +0000

    add custom editor for param saving

---
+++ Assets/Code/WorldParams.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Code
+{
+    [CustomEditor(typeof(World))]
+    public class WorldParams : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            if (GUILayout.Button("Save Params to File"))
+            {
+                Params param = new Params();
+                param.saveToFile();
+            }
+        }
+    }
+}
