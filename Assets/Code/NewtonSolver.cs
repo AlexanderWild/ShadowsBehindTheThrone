b2f5434c75dcdd72e16df3aa14827603782c94eb
Wyatt <git.wquist@gmail.com>
2020-08-29 21:17:25 +0100

    add very bad repulsion forces

--- Assets/Code/NewtonSolver.cs
+++ Assets/Code/NewtonSolver.cs
@@ -13,7 +13,8 @@ namespace Assets.Code
 		}
 
 		public const float SPRING = 0.01f;
-		public const float REPULSION_DISTANCE = 0.5f;
+		public const float DAMPING = 0.8f;
+		public const float REPULSION_DISTANCE = 0.2f;
 
 		public List<T> nodes;
 		public IDistanceMetric metric;
@@ -24,20 +25,36 @@ namespace Assets.Code
 			metric = m;
 
 			foreach (T n in nodes)
-				n.transform.position = Vector3.zero;
+			{
+				Vector3 p = new Vector3();
+				p.x = (float)Eleven.random.NextDouble();
+				p.y = (float)Eleven.random.NextDouble();
+
+				n.transform.position = p;
+			}
 		}
 
 		public void solve()
 		{
-			for (int i = 0; i != nodes.Count - 1; ++i)
+			for (int i = 0; i != nodes.Count; ++i)
 			{
-				for (int j = i + 1; j != nodes.Count; ++j)
-				{
-					T a = nodes[i];
-					T b = nodes[j];
+				T a = nodes[i];
+				Vector3 force = Vector3.zero;
 
-					//
+				for (int j = 0; j != nodes.Count; ++j)
+				{
+					T b = nodes[j];
+					if (a == b)
+						continue;
+
+					Vector3 dist = (b.transform.position - a.transform.position);
+					float magsqr = dist.sqrMagnitude;
+
+					if (magsqr != 0.0f && magsqr < REPULSION_DISTANCE)
+						force += dist / magsqr * SPRING;
 				}
+
+				a.transform.position += force * DAMPING;
 			}
 		}
 	}
