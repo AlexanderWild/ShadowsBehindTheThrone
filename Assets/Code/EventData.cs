2cea230191ac8793b2efcce2c274604524ef2108
Wyatt <git.wquist@gmail.com>
2021-03-14 04:21:21 +0000

    define event system json format

---
+++ Assets/Code/EventData.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code
+{
+	[Serializable]
+    public class EventData
+    {
+		[Serializable]
+		public class Effect
+		{
+			public enum Type
+			{
+				ADD_POWER,
+				WRITE
+			}
+
+			public string type;
+			public string value;
+			
+			//
+		}
+
+		[Serializable]
+		public class Outcome
+		{
+			public string name;
+			public string description;
+
+			public List<Effect> effects;
+
+			//
+		}
+
+		public enum Type { LOCATION, UNIT, WORLD }
+		public string type;
+	
+        public string conditional;
+		public double probability;
+
+		public string modCredit;
+		public string imgCredit;
+
+		public List<Outcome> outcomes;
+
+		//
+    }
+}
